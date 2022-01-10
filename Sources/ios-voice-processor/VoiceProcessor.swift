65d93a3e4bfe8045fd1b6056e10d400a52d3ded8
Fred RAJAONA <fred@kannelle.io>
2022-01-10 17:29:22 +0100

    feat(SPM): implement support for SPM

---
+++ Sources/ios-voice-processor/VoiceProcessor.swift
@@ -0,0 +1,1 @@
+../../VoiceProcessor.swift
---
+++ Tests/ios-voice-processorTests/ios_voice_processorTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ios_voice_processor
+
+final class ios_voice_processorTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ios_voice_processor().text, "Hello, World!")
+    }
+}
